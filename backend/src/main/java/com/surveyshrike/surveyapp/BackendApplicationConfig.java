a2b54790451ff03cd594e4e9fe8cb23f61ccb571
anandpillai <anandpillai@192.168.31.59>
2019-10-14 02:00:10 +0530

    [Changes] - for swagger documentation

--- backend/src/main/java/com/surveyshrike/surveyapp/BackendApplicationConfig.java
+++ backend/src/main/java/com/surveyshrike/surveyapp/BackendApplicationConfig.java
@@ -1,5 +1,7 @@
 package com.surveyshrike.surveyapp;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +9,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
@@ -28,6 +36,22 @@ public class BackendApplicationConfig {
 		config.addAllowedMethod("DELETE");
 		source.registerCorsConfiguration("/**", config);
 		return new CorsFilter(source);
+	}
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.surveyshrike")).paths(PathSelectors.any()).build()
+				.apiInfo(getApiInfo());
+	}
+
+	private ApiInfo getApiInfo() {
+		return new ApiInfo("Survey Shrike Survey APIs",
+				"APIs created for sample survey for SurveyShrike. The user can submit a survey response and view all the responses",
+				"v1", "http://swagger.io/terms/",
+				new Contact("Anand PILLAI", "https://github.com/jprexus", "anandjpillai@gmail.com"),
+				"Licensed Under Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0.html",
+				Collections.emptyList());
 	}
 
 }
--- backend/src/main/java/com/surveyshrike/surveyapp/controller/SurveyController.java
+++ backend/src/main/java/com/surveyshrike/surveyapp/controller/SurveyController.java
@@ -31,19 +31,20 @@ public class SurveyController {
 	@Autowired
 	private ISurveyService surveyService;
 
-	@ApiOperation(value = "Get All Surveys", response = List.class)
-	@ApiResponses(value = { @ApiResponse(code = 200, message = "All Survey Responses", response = List.class),
+	@ApiOperation(value = "Get All Survey Responses", response = List.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Return All Survey Responses received so far", response = List.class),
 			@ApiResponse(code = 500, message = "Internal Server Error") })
-	@GetMapping("/surveys")
+	@GetMapping("/v1/surveys")
 	public List<SurveyBO> getSurveys() {
 		// Find all surveys
 		return this.surveyService.findAll();
 	}
 
-	@ApiOperation(value = "Post a Survey")
-	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully posted a Survey"),
+	@ApiOperation(value = "Submit a Survey Response")
+	@ApiResponses(value = { @ApiResponse(code = 201, message = "Successfully submitted a Survey Response"),
 			@ApiResponse(code = 500, message = "Internal Server Error") })
-	@PutMapping("/surveys")
+	@PutMapping("/v1/survey")
 	public ResponseEntity<SurveyBO> addSurvey(@RequestBody SurveyBO surveyBO) {
 		this.surveyService.save(surveyBO);
 		return new ResponseEntity<>(HttpStatus.CREATED);
