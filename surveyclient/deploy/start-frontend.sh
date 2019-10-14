c282c75ca480e996b89dcda7e064286806a70029
Anand Pillai <anandpillai@raris-MacBook-Air.local>
2019-10-14 09:21:07 +0530

    Deploy frontend

---
+++ surveyclient/deploy/start-frontend.sh
@@ -0,0 +1,6 @@
+cd /apps/surveyshrike/surveyapp/frontend/apache-tomcat-8.5.46/webapps
+rm -rf surveyapp
+tar -xvf surveyapp.tar
+cd /apps/surveyshrike/surveyapp/frontend/apache-tomcat-8.5.46/bin
+sudo su
+./startup.sh
---
+++ surveyclient/deploy/stop-frontend.sh
@@ -0,0 +1,3 @@
+cd /apps/surveyshrike/surveyapp/frontend/apache-tomcat-8.5.46/bin
+sudo su
+./shutdown.sh
